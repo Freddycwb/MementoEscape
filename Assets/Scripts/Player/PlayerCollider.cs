0020a61ebd60fd6e23cd2e6dce8111e144ed4858
Freddy Matheus <45660002+Freddycwb@users.noreply.github.com>
2023-04-22 19:43:30 -0300

    New Store, Starting Damage Code, etc...

--- Assets/Scripts/Player/PlayerCollider.cs
+++ Assets/Scripts/Player/PlayerCollider.cs
@@ -4,13 +4,42 @@ using UnityEngine;
 
 public class PlayerCollider : MonoBehaviour
 {
+    private Rigidbody rb;
+    private float ySpeed;
+
+    [SerializeField] private Vector2 ResistAndDeathVelocity;
+    [SerializeField] private float hp;
+
     public GameEvent reachFinish;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void Update()
+    {
+        ySpeed = rb.velocity.y != 0 ? rb.velocity.y : ySpeed;
+    }
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Exit"))
         {
             reachFinish.Raise();
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.layer == 3)
+        {
+            float damage = 0;
+            if (ySpeed < ResistAndDeathVelocity.x)
+            {
+                damage = ySpeed;
+            }
+            Debug.Log("speed: " + ySpeed + " Damage: " + damage);
         }
     }
 }
